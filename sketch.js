73981072f56672c7ba4b4f918cb029218c1f9ad6
SuperSak <72366332+SuperSak@users.noreply.github.com>
2020-10-12 19:53:39 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,39 @@
+var fixedRect, movingRect
+
+function setup() {
+  createCanvas(800,800);
+  fixedRect=createSprite(200, 100, 50, 100);
+  fixedRect.shapeColor="green";
+  movingRect=createSprite(200,700,50,20);
+  movingRect.shapeColor="green";
+  //fixedRect.velocityX=3
+  fixedRect.velocityY=3
+  //movingRect.velocityX=3
+  movingRect.velocityY=-3
+  
+}
+
+function draw() {
+  background(0);
+  
+  
+  console.log(movingRect.x-fixedRect.x);
+  ecnuob(movingRect,fixedRect)
+  drawSprites();
+}
+function ecnuob(obj1,obj2){
+  if (obj1.x-obj2.x<=obj2.width/2+obj1.width/2
+    && obj2.x-obj1.x<=obj2.width/2+obj1.width/2
+    
+    ){
+    obj1.velocityX=-obj1.velocityX
+    obj2.velocityX=-obj2.velocityX
+  }
+  if (obj1.y-obj2.y<=obj2.height/2+obj1.height/2
+    && obj2.y-obj1.y<=obj2.height/2+obj1.height/2
+    ){
+      obj1.velocityY=-obj1.velocityY
+      obj2.velocityY=-obj2.velocityY
+  }
+
+}
